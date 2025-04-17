c247d2cea8b72bc4cec9fa3fe664a3b1f1f29155
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-17 13:45:54 +0200

    [UPD] created home path

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const express  = require('express');
 //inizializzo express
 const app = express();
 
+//inizializzo la porta
+const port = 3000;
+
+//home
+app.get('/', (req,res) =>{
+    res.send('Home');
+});
